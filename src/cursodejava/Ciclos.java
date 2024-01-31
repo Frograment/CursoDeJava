d20ac924f788b6519c825d3042da8bd1b32633de
Frograment <frograment@gmail.com>
2024-01-30 18:58:59 -0600

    Ciclos For y While

--- src/cursodejava/Ciclos.java
+++ src/cursodejava/Ciclos.java
@@ -15,7 +15,43 @@ public class Ciclos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Ciclos
+        //For, While, Do While
+        
+        //Ciclo for es cuando sabemos el numero de iteraciones
+        
+        //Ciclo While es cuando no sabemos el numero de iteraciones
+        
+//        for(int i = 1; i<10; i=i+2){
+//            System.out.println("Iteracion "+i);
+//        }
+//        String tipo;
+//        int edad = 18;
+//        if(edad<18){
+//            tipo = "Menor de edad";
+//        }else{
+//            tipo = "Mayor de edad";
+//        }
+//        
+//        System.out.println(tipo);
+
+
+        int factorial = 5;
+        int resultado;
+        resultado = 1;
+        for (int i = 1; i <= factorial; i++) {
+            resultado = resultado*i;
+             System.out.println(resultado);
+        }
+        
+       
+        
+//        int i = 0;
+//        while(i < 100){
+//            System.out.println(i);
+//            i += 5;
+//        }
+//        //5*4*3*2*1
     }
     
 }
